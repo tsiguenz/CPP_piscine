ba94a2476875b638fb7265b5f9eb987168d278d9
Thibaut SIGUENZA <tsiguenz@e3r13p7.clusters.42paris.fr>
2022-05-11 10:13:23 +0200

    clean repo

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -6,14 +6,25 @@
 /*   By: tsiguenz <tsiguenz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 17:55:39 by tsiguenz          #+#    #+#             */
-/*   Updated: 2022/05/09 18:05:26 by tsiguenz         ###   ########.fr       */
+/*   Updated: 2022/05/10 16:00:42 by tsiguenz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cstring>
 
-int	main(void)
+int	main(int ac, char **av)
 {
-	std::cout << "test" << std::endl;
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; i < ac; i++)
+	{
+		for (size_t y = 0; y < strlen(av[i]); y++)
+			av[i][y] = std::toupper(av[i][y]);
+		std::cout << av[i];
+		if (i != ac - 1)
+			std::cout << " ";
+	}
+	std::cout << std::endl;
 	return (0);
 }
