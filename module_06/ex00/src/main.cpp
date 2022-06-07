bc709b0873de50c6e700c76d9fb3a1a064ee485e
Thibaut SIGUENZA <tsiguenz@e3r7p17.42.fr>
2022-06-07 20:14:37 +0200

    mod06 ex00 fix

--- module_06/ex00/src/main.cpp
+++ module_06/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tsiguenz <tsiguenz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 14:47:05 by tsiguenz          #+#    #+#             */
-/*   Updated: 2022/06/07 20:11:37 by tsiguenz         ###   ########.fr       */
+/*   Updated: 2022/06/07 20:13:49 by tsiguenz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 void	scalarConversion(std::string str) {
 	if (str.empty() == true)
 		return ;
-	std::cout << "------------------------------" << std::endl;
-	std::cout << "DEBUG str = [" << str << "]" << std::endl;
+//	std::cout << "------------------------------" << std::endl;
+//	std::cout << "DEBUG str = [" << str << "]" << std::endl;
 	if (isChar(str))
 		printNumbers(str.at(0));
 	else if (isInt(str))
@@ -29,57 +29,57 @@ void	scalarConversion(std::string str) {
 		std::cerr << "Bad argument" << std::endl;
 }
 
-//int	main(int ac, char **av) {
-//	if (ac != 2) {
-//		std::cerr << "Bad number of arguments" << std::endl;
-//		return (1);
-//	}
-//	scalarConversion(av[1]);
-//	return (0);
-//}
-
-int	main() {
-	std::cout << "########## char conversion : ##########" << std::endl;
-	scalarConversion("t");
-	scalarConversion("Z");
-	std::cout << "########## int conversion : ##########" << std::endl;
-	scalarConversion("0");
-	scalarConversion("42");
-	scalarConversion("-42");
-	scalarConversion("2147483647");
-	scalarConversion("-2147483648");
-	std::cout << "########## float conversion : ##########" << std::endl;
-	scalarConversion("42.42f");
-	scalarConversion("42.0f");
-	scalarConversion("-42.0f");
-	scalarConversion("9223372036854775807.0f");
-	scalarConversion("-2147483649");
-	scalarConversion("-inff");
-	scalarConversion("+inff");
-	scalarConversion("nanf");
-	std::cout << "########## double conversion : ##########" << std::endl;
-	scalarConversion("42.42");
-	scalarConversion("42.0");
-	scalarConversion("-42.0");
-	scalarConversion("9223372036854775808.0");
-	scalarConversion("10000000000000000000000000000000000000000.0");
-	scalarConversion("-inf");
-	scalarConversion("+inf");
-	scalarConversion("nan");
-	std::cout << "########## bad conversion : ##########" << std::endl;
-	scalarConversion("");
-	scalarConversion(" ");
-	scalarConversion("#");
-	scalarConversion("asdfdas");
-	scalarConversion("42.");
-	scalarConversion("42.f");
-	scalarConversion("42f");
-	scalarConversion("2147483648");
-	scalarConversion("-2147483649");
-	scalarConversion("inf");
-	scalarConversion("nan    ");
-	scalarConversion("10000000000000000000000000000000000000000.0f");
-	errno = 0;
-	scalarConversion("1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111.0");
+int	main(int ac, char **av) {
+	if (ac != 2) {
+		std::cerr << "Bad number of arguments" << std::endl;
+		return (1);
+	}
+	scalarConversion(av[1]);
 	return (0);
 }
+
+//int	main() {
+//	std::cout << "########## char conversion : ##########" << std::endl;
+//	scalarConversion("t");
+//	scalarConversion("Z");
+//	std::cout << "########## int conversion : ##########" << std::endl;
+//	scalarConversion("0");
+//	scalarConversion("42");
+//	scalarConversion("-42");
+//	scalarConversion("2147483647");
+//	scalarConversion("-2147483648");
+//	std::cout << "########## float conversion : ##########" << std::endl;
+//	scalarConversion("42.42f");
+//	scalarConversion("42.0f");
+//	scalarConversion("-42.0f");
+//	scalarConversion("9223372036854775807.0f");
+//	scalarConversion("-2147483649");
+//	scalarConversion("-inff");
+//	scalarConversion("+inff");
+//	scalarConversion("nanf");
+//	std::cout << "########## double conversion : ##########" << std::endl;
+//	scalarConversion("42.42");
+//	scalarConversion("42.0");
+//	scalarConversion("-42.0");
+//	scalarConversion("9223372036854775808.0");
+//	scalarConversion("10000000000000000000000000000000000000000.0");
+//	scalarConversion("-inf");
+//	scalarConversion("+inf");
+//	scalarConversion("nan");
+//	std::cout << "########## bad conversion : ##########" << std::endl;
+//	scalarConversion("");
+//	scalarConversion(" ");
+//	scalarConversion("#");
+//	scalarConversion("asdfdas");
+//	scalarConversion("42.");
+//	scalarConversion("42.f");
+//	scalarConversion("42f");
+//	scalarConversion("2147483648");
+//	scalarConversion("-2147483649");
+//	scalarConversion("inf");
+//	scalarConversion("nan    ");
+//	scalarConversion("10000000000000000000000000000000000000000.0f");
+//	errno = 0;
+//	scalarConversion("1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111.0");
+//	return (0);
+//}
