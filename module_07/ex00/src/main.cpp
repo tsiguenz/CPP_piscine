7b0723e087146cb22c8375322bb52612004a1964
Thibaut SIGUENZA <tsiguenz@e3r7p17.42.fr>
2022-06-08 15:30:50 +0200

    mod07 ex00 use mandatory main

--- module_07/ex00/src/main.cpp
+++ module_07/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tsiguenz <tsiguenz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 14:47:05 by tsiguenz          #+#    #+#             */
-/*   Updated: 2022/06/08 15:26:14 by tsiguenz         ###   ########.fr       */
+/*   Updated: 2022/06/08 15:30:03 by tsiguenz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,43 +14,17 @@
 #include "template.h"
 
 int	main() {
-	std::cout << "---------- Test swap : ----------" << std::endl;
-	{
-		int	i = 5;
-		int	j = 42;
-		std::string	s1 = "Hello";
-		std::string	s2 = "World";
-
-		std::cout << "i = " << i << std::endl;
-		std::cout << "j = " << j << std::endl;
-		swap<int>(i, j);
-		std::cout << "i = " << i << std::endl;
-		std::cout << "j = " << j << std::endl;
-		std::cout << "s1 = " << s1 << std::endl;
-		std::cout << "s2 = " << s2 << std::endl;
-		swap(s1, s2);
-		std::cout << "s1 = " << s1 << std::endl;
-		std::cout << "s2 = " << s2 << std::endl;
-	}
-	std::cout << "---------- Test min : ----------" << std::endl;
-	{
-		int	i = 5;
-		int	j = 42;
-		double	x = 5.0f;
-		double	y = 5.0f;
-
-		std::cout << min<int>(i, j) << std::endl;
-		std::cout << min<float>(x, y) << std::endl;
-	}
-	std::cout << "---------- Test min : ----------" << std::endl;
-	{
-		int	i = 5;
-		int	j = 42;
-		double	x = 5.0f;
-		double	y = 5.0f;
-
-		std::cout << max<int>(i, j) << std::endl;
-		std::cout << max<float>(x, y) << std::endl;
-	}
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 	return (0);
 }
