1553a990accbe621a570f0e663d3ff3172953c8d
Thibaut SIGUENZA <tsiguenz@e3r6p16.42.fr>
2022-06-09 09:47:05 +0200

    mod07 ex02 finished

--- module_07/ex02/src/Array.hpp
+++ module_07/ex02/src/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: tsiguenz <tsiguenz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 16:33:38 by tsiguenz          #+#    #+#             */
-/*   Updated: 2022/06/08 19:08:37 by tsiguenz         ###   ########.fr       */
+/*   Updated: 2022/06/09 09:43:02 by tsiguenz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,25 +22,34 @@ class Array {
 
 		Array(unsigned int size): size_(size) { this->array_ = new T[size]; }
 
-		Array(Array<T> const& array) { *this = array; }
+		Array(Array<T> const& array): array_(), size_(0) { *this = array; }
 
 		~Array() { delete [] this->array_; }
 
 		Array&	operator=(Array<T> const& array) {
+			delete [] this->array_;
+			this->array_ = new T[array.size_];
 			this->size_ = array.size_;
-			this->array_ = array.array_;
+			for (unsigned int i = 0; i < array.size_; i++)
+				this->array_[i] = array.getElement(i);
 			return *this;
 		}
+
 		int	size() { return this->size_; }
+
 		T&	operator[](unsigned int i) {
 			if (i < 0 || i >= this->size_)
-				throw std::out_of_range("Out of range\n");
+				throw std::out_of_range("Out of range");
 			return this->array_[i];
 		}
 
 	private:
 		T*				array_;
 		unsigned int	size_;
+
+		T	getElement(unsigned int i) const {
+			return this->array_[i];
+		}
 };
 
 #endif // ARRAY_HPP
--- module_07/ex02/src/main.cpp
+++ module_07/ex02/src/main.cpp
@@ -6,80 +6,47 @@
 /*   By: tsiguenz <tsiguenz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/31 14:47:05 by tsiguenz          #+#    #+#             */
-/*   Updated: 2022/06/08 19:11:16 by tsiguenz         ###   ########.fr       */
+/*   Updated: 2022/06/09 09:46:25 by tsiguenz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <iostream>
-//#include "Array.hpp"
-//
-//int	main() {
-//	try {
-//		Array<int>	a(10);
-//		Array<int>	b;
-//
-//		a[0] = 42;
-//		std::cout << a.size() << std::endl;
-//		std::cout << a[0] << std::endl;
-//		std::cout << b.size() << std::endl;
-//		a[-1];
-//	}
-//	catch (std::exception const& e) {
-//		std::cout << "Exception catch : " << e.what();
-//	}
-//	return 0;
-//}
 #include <iostream>
-#include <cstdlib>
 #include "Array.hpp"
 
-#define MAX_VAL 750
-int main(int, char**)
-{
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+int	main() {
+	try {
+		Array<char>	a;
+		std::cout << a[0] << std::endl;
+	} catch (std::exception& e) {
+		std::cout << "Exception catch : " << e.what() << std::endl;
+	}
+	try {
+		Array<int>	a(10);
+		a[0] = 42;
+		Array<int>	b(a);
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+		std::cout << a.size() << std::endl;
+		std::cout << b.size() << std::endl;
+		std::cout << a[0] << std::endl;
+		std::cout << b[0] << std::endl;
+		a[-1];
+	}
+	catch (std::exception& e) {
+		std::cout << "Exception catch : " << e.what() << std::endl;
+	}
+	try {
+		Array<float>	a(10);
+		a[5] = 42.42f;
+		Array<float>		b(a);
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;//
-    return 0;
+		std::cout << a.size() << std::endl;
+		std::cout << b.size() << std::endl;
+		std::cout << a[5] << std::endl;
+		std::cout << b[5] << std::endl;
+		a[-1];
+	}
+	catch (std::exception& e) {
+		std::cout << "Exception catch : " << e.what() << std::endl;
+	}
+	return 0;
 }
